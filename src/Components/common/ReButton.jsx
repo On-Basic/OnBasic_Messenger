b7d8e8db4bfc81754aaf9c1dd941d5cc7383ea95
dahye-program <dahye0729@sch.ac.kr>
2022-02-11 15:49:24 +0900

    Add : 삭제, 답장 컴포넌트 추가

---
+++ src/Components/common/ReButton.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { PALLETS } from 'Styles/theme';
+import { ReplayICON } from 'Assets/Icons';
+import { RemoveICON } from 'Assets/Icons';
+
+const ReButton = ({ReplayFunc, RemoveFunc}) => {
+  return (
+    <>
+    <ReplayBtn onClick={ReplayFunc}><ReIcon src={ReplayICON}></ReIcon></ReplayBtn>
+    <RemoveBtn onClick={RemoveFunc}><ReIcon src={RemoveICON}></ReIcon></RemoveBtn>
+    </>
+  )
+}
+
+export default ReButton;
+
+ReButton.propTypes={
+  ReplayFunc: PropTypes.func,
+  RemoveFunc: PropTypes.func,
+};
+
+ReButton.defaultProps={
+  ReplayFunc: ()=>{},
+  RemoveFunc: ()=>{},
+};
+
+const ReIcon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+const ReplayBtn = styled.button`
+  background-color: ${PALLETS.LIGHTGRAY};
+  border: none;
+`;
+
+const RemoveBtn = styled.button`
+  background-color: ${PALLETS.LIGHTGRAY};
+  border: none;
+`;
