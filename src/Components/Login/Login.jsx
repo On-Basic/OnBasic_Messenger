f98e2670cf3d8e49de7496f0902b5ff87f5e53ef
dahye-program <dahye0729@sch.ac.kr>
2022-02-11 17:28:27 +0900

    Add : Redux 사용 유저 로그인

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import {Input} from 'Components/common/Input';
+import Button from 'Components/common/Button';
+import { PALLETS } from 'Styles/theme';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    userId: '',
+    userName: '',
+  });
+
+  const {userId, userName} = userData;
+
+  const handleUserInput = e => {
+    const {name, value} = e.target;
+    setUserData({...userData, [name]:value});
+  };
+
+  const submitCheck = userId.length > 0 && userName.length > 0 
+
+  const submitUserData = () => {
+    dispatch({
+      type: 'USER_LOGIN',
+      userId: userId,
+      userName: userName,
+    });
+    submitCheck ? alert(`${userName}`+'님 환영합니다!!') : alert('아이디와 이름 모두 입력해주세요.');
+    navigate('/main');
+  };
+
+  return (
+  <>
+    <Input
+      type="text"
+      name="userId"
+      placeholder="Id를 입력하세요."
+      onChange={handleUserInput} 
+    />
+    <Input
+    type="text"
+    name="userName"
+    placeholder="이름을 입력하세요."
+    onChange={handleUserInput} 
+    />
+    <Button
+      Width="495px"
+      Height="100px"
+      fontSize="45px"
+      backgroundColor= {PALLETS.BLUE}
+      Color={PALLETS.WHITE}
+      Content="Login"
+      ClickFunc={submitUserData}
+      isActive={submitCheck}
+    />
+  </>
+  )
+}
+
+export default Login;
