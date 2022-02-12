c1df874921b3a4c8cadbed205d128aa8428548e6
dahye-program <dahye0729@sch.ac.kr>
2022-02-12 16:42:39 +0900

    Add : 메시지 전송 기능 구현

--- src/Components/Main/Chat.jsx
+++ src/Components/Main/Chat.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import { PALLETS } from 'Styles/theme';
 import { Input } from 'Components/common/Input';
 import Button from 'Components/common/Button';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ChatContentList from './ChatContentList';
 
 const Chat = () => {
+  const dispatch = useDispatch();
+
+  const userId = useSelector(state=>state.login.userId);
+  const userName = useSelector(state=>state.login.userName);
   const chatList = useSelector((state) => state.message);
+
+  const [InputMessage, setInputMessage] = useState();
+
+  const onChange = e => {
+    setInputMessage(e.target.value);
+  };
+
+  const Send = () => {
+    dispatch({
+      type: 'SEND_CHAT',
+      userId: userId,
+      userName: userName,
+      message: InputMessage,
+    });
+    setInputMessage('');
+  };
+
+  const pressEnter = e => {
+    if (e.key === 'Enter') {
+      Send();
+      setInputMessage('');
+    }
+  };
 
   return (
     <ChatBar>
@@ -22,9 +49,18 @@ const Chat = () => {
         <Input
           type="text"
           name="ChatContent"
+          value={InputMessage}
           placeholder="메시지를 입력하세요."
+          onChange={onChange}
+          onKeyPress={pressEnter}
         />
-        <Button width="130px" height="50px" fontSize="25px" Content="전송" />
+        <Button 
+          width="130px" 
+          height="50px" 
+          fontSize="25px" 
+          Content="전송"
+          ClickFunc={Send} 
+        />
       </InputContent>
     </ChatBar>
   );
